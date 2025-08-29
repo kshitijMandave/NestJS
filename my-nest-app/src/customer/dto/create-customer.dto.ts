b075ad501644fbe6f1b9e3fd3b0cd1db3a729b31
kshitijMandave <kshitij.mandwe@gmail.com>
2025-08-29 23:43:12 +0530

    Day 7 of Nest JS, where revised all the DTO Topic and trying onece again

--- my-nest-app/src/customer/dto/create-customer.dto.ts
+++ my-nest-app/src/customer/dto/create-customer.dto.ts
@@ -1,5 +1,10 @@
 /* eslint-disable prettier/prettier */
+import { IsInt, IsString } from 'class-validator';
+
+/* eslint-disable prettier/prettier */
 export class CreateCustomerDto {
+  @IsString()
   name: string;
+  @IsInt()
   age: number;
 }
